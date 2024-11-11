4115e5f2ab5e280482e88339056e85f181b6594f
enasskadree <enasskadree2003@gmail.com>
2024-11-12 00:21:06 +0300

    create home repo impl

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,53 @@
+import 'package:bookly_app/Features/home/data/data%20sources/home_locale_data_source.dart';
+import 'package:bookly_app/Features/home/data/data%20sources/home_remote_data_source.dart';
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl extends HomeRepo
+{
+  final HomeLocaleDataSource homeLocaleDataSource;
+  final HomeRemoteDataSource homeRemoteDataSource;
+
+  HomeRepoImpl({required this.homeLocaleDataSource, required this.homeRemoteDataSource});
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async
+  {
+    try
+    {
+      List<BookEntity> books;
+      books = homeLocaleDataSource.fetchFeaturedBooks();
+      if(books.isNotEmpty)
+      {
+        return right(books);
+      }
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    }
+    catch(e)
+    {
+      return left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async{
+    try
+    {
+      List<BookEntity> books;
+      books = homeLocaleDataSource.fetchNewestBooks();
+      if(books.isNotEmpty)
+      {
+        return right(books);
+      }
+      books = await homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
+    }
+    catch(e)
+    {
+      return left(Failure());
+    }
+  }
+
+}
